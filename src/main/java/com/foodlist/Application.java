42fff62491df8ae5fa37953597427719d8216584
GrigoryKrasovsky <grigory.krasovsky@gmail.com>
2022-12-28 20:39:41 +0400

    trying to make modules

--- src/main/java/com/foodlist/Application.java
+++ src/main/java/com/foodlist/Application.java
@@ -2,8 +2,10 @@ package com.foodlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaServer
 @SpringBootApplication
 @EnableJpaRepositories
 public class Application {
